0142e92d15719e3d8451af8282f0783f967d1511
Mariia <pandrakmaria09@gmail.com>
2026-09-11 17:41:13 +0300

    06_homeworkk

--- HOMEWORK_07/HOMEWORK_07.cpp
+++ HOMEWORK_07/HOMEWORK_07.cpp
@@ -1,7 +1,106 @@
-
 #include <iostream>
-
+#include <iomanip> 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+
+    //1
+
+    /*
+    const int rows = 3;
+    const int cols = 4;
+    int a[rows][cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            a[i][j] = rand() % 5;
+        }
+    }
+
+    cout << "\n--  Masiv  --" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << left << setw(4) << a[i][j];
+        }
+        cout << endl;
+    }
+
+    int count = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                count = count + 1;
+            }
+        }
+    }
+    cout << endl;
+    cout << "--" << "    " << count << "    " << "--" << endl;
+    */
+
+
+    //2
+
+
+
+    int n;
+    cout << "Vvedit rozmir matrytsi n: ";
+    cin >> n;
+
+    int a[20][20];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            a[i][j] = -50 + rand() % 100;
+        }
+    }
+
+    cout << "\n--  Matrytsia  --" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << left << setw(5) << a[i][j];
+        }
+        cout << endl;
+    }
+
+    int maxElement = a[1][0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > j)
+            {
+                if (a[i][j] > maxElement)
+                {
+                    maxElement = a[i][j];
+                }
+            }
+        }
+    }
+
+    cout << endl;
+    cout << "--" << maxElement << "  --" << endl;
+
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ homewok_06/homewok_06.cpp
@@ -0,0 +1,125 @@
+
+#include <iostream>
+using namespace std;
+#include <iomanip> 
+
+int main()
+{
+    ////1
+    //int n;
+    //cout << "Vvedit n: ";
+    //cin >> n;
+
+    //for (int i = 1; i <= 3; i++)
+    //{
+    //    for (int j = 1; j <= i * n; j++)
+    //    {
+    //        cout << i;
+    //    }
+    //    cout << endl;
+    //}
+
+    //2
+    //const int n = 10;
+    //for (int i = 0; i < n; i++)
+    //{
+    //    for (int j = 0; j < n; j++)
+    //    {
+    //        if (i == j)
+    //        {
+    //            cout << 0;
+    //        }
+    //        else
+    //        {
+    //            cout << 1;
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+    //3
+
+    //int sum = 0;
+    //for (int k = 10; k >= 1; k--)
+    //{
+    //    for (int j = 1; j <= k; j++)
+    //    {
+    //        cout << k << " ";
+    //        sum = sum + k;
+    //    }
+    //    cout << endl;
+    //}
+    //cout << endl;
+    //cout << "Suma: " << sum << endl;
+
+    ////4
+    //for (int i = 1; i <= 15; i++)
+    //{
+    //    if (i % 2 == 0)
+    //    {
+    //        for (int j = 1; j <= 8; j++)
+    //        {
+    //            cout << i << " ";
+    //        }
+    //    }
+    //    else
+    //    {
+    //        for (int j = 1; j <= 10; j++)
+    //        {
+    //            cout << 1 << " ";
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+    ////5
+
+    //for (int i = 1; i <= 30; i++)
+    //{
+    //    if (i % 2 != 0)
+    //    {
+    //        for (int j = 1; j <= i; j++)
+    //        {
+    //            cout << j << " ";
+    //        }
+    //    }
+    //    else
+    //    {
+    //        for (int j = 1; j <= 5; j++)
+    //        {
+    //            cout << 1 << " ";
+    //        }
+    //    }
+    //    cout << endl;
+    //}
+
+    //6
+    //for (int i = 1; i <= 10; i++)
+    //{
+    //    for (int j = 1; j <= 10; j++)
+    //    {
+    //        cout << left << setw(5) << i * j;
+    //    }
+    //    cout << endl;
+    //}
+
+    ////7
+    //int a, b;
+    //cout << "Vvedit a: ";
+    //cin >> a;
+    //cout << "Vvedit b: ";
+    //cin >> b;
+
+    //int count = 0;
+    //for (int i = a; i <= b; i++)
+    //{
+    //    if (i % 12 == 0)
+    //    {
+    //        count = count + 1;
+    //    }
+    //}
+    //cout  << count << endl;
+
+
+}
+
